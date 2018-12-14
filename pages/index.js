f649ba738c4d3c2505fe2af4362498a3d4497e7d
PaulStafford1997 <32099821+paulstafford1997@users.noreply.github.com>
2018-12-14 13:39:14 +0000

    Original repo

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+// This is the Link API
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+
+// Pass this content as 'props' to child components
+const Index = props => (
+  <div>
+    <ul>
+      <li><Link href="/index"><a>Home</a></Link></li>
+      <li><Link href="/news"><a>Irish News</a></Link></li>
+      <li><Link href="/business"><a>Business News</a></Link></li>
+      <li><Link href="/sport"><a>Sport News</a></Link></li>
+      <li><Link href="/ign"><a>Gaming News</a></Link></li>
+    </ul>
+  </div>
+);
+
+// see https://nextjs.org/learn/basics/fetching-data-for-pages
+
+Index.getInitialProps = async function() {
+
+  const url = 'https://api.tvmaze.com/search/shows?q=batman';
+
+  const res = await fetch(url)
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index
---
+++ pages/news.js
@@ -0,0 +1,236 @@
+// This is the Link API
+import Link from 'next/link';
+
+import fetch from 'isomorphic-unfetch';
+
+import SearchForm from '../components/SearchForm';
+
+const defaultNewsSource = 'the-irish-times';
+
+const apiKey = '62409199c79d461ea0a6fd6f83b8d44d';
+
+async function getNews(url){
+  try{
+    const res = await fetch(url);
+
+    const data = await res.json();
+
+    return (data);
+  }
+  catch(error){
+    return (error);
+  }
+}
+
+export default class News extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {
+      newsSource: "",
+      url: "",
+      articles: []
+    }
+  }
+
+  setNewsSource = (input) => {
+    this.setState({
+      newsSource: input,
+      url: `https://newsapi.org/v2/top-headlines?source=${input}$apiKey=${apiKey}`
+    })
+  }
+
+  render(){
+    if(this.state.articles.length == 0){
+      this.state.articles = this.props.articles;
+    }
+    return(
+      <div>
+
+        <SearchForm setNewsSource={this.setNewsSource}/>
+
+        <ul className="newsMenu">
+          <li><a href="#" onClick={this.searchNewsAPI} name="top-headlines?country=ie">Top Headlines Ireland</a></li>
+          <li><a href="#" onClick={this.searchNewsAPI} name="top-headlines?country=ie&category=business">Business News Ireland</a></li>
+          <li><a href="#" onClick={this.searchNewsAPI} name="everything?q=technology">Technology News</a></li>
+          <li><a href="#" onClick={this.searchNewsAPI} name="top-headlines?country=ie&category=weather">Weather in Ireland</a></li>
+        </ul>
+
+        <div>
+          {/* Iterate through articles using Array map */}
+          {/* Display author, publishedAt, image, description, and content */}
+          {/* for each story. Also a link for more.. */}
+          {this.state.articles.map((article, index) =>(
+            <section key={index}>
+            <h3>{article.title}</h3>
+            <p className="author">{article.author} {formatDate(article.publishedAt)}</p>
+            <img src={article.urlToImage} alt="article image" className="img-article"></img>
+            <p>{article.description}</p>
+            <p>{article.content}</p>
+            <p><Link href="/story"><a>Read More</a></Link></p>
+            <p onClick={this.test}>click..</p>
+            </section>
+          ))}
+        </div>
+        <style jsx>{`
+              /* CSS for this page */
+              section {
+                width: 50%;
+                border: 1px solid gray;
+                background-color: rgb(240, 248, 255);
+                padding: 1em;
+                margin: 1em;
+              }
+
+            .author {
+                font-style: italic;
+                font-size: 0.8em;
+              }
+            .img-article {
+                max-width: 50%;
+              }
+
+            .newsMenu {
+              display: flex;
+              flex-direction: row;
+              margin: 0;
+              padding: 0;
+              margin-top: 20px;
+            }
+            .newsMenu li {
+              display: inline-table;
+              padding-left: 20px;
+            }
+
+            .newsMenu li a {
+              font-size: 1em;
+              color: blue;
+              display: block;
+              text-decoration: none;
+            }
+
+            .newsMenu li a:hover {
+              color: rgb(255, 187, 0);
+              text-decoration: underline;
+            }
+          `}</style>
+      </div>
+    );
+  }
+
+  static async getInitialProps(response){
+
+    const initUrl = `https://newsapi.org/v2/top-headlines?sources=${defaultNewsSource}&apiKey=${apiKey}`;
+
+    const data = await getNews(initUrl);
+
+    if(Array.isArray(data.articles)){
+      return{
+        articles: data.articles
+      }
+    }
+    else{
+      console.error(data)
+      if(response){
+        response.statusCode = 400
+        response.end(data.message);
+      }
+    }
+  }
+  
+
+  async componentDidUpdate(_prevProps, prevState){
+
+    if(this.state.url !== prevState.url){
+
+      const data = await getNews(this.state.url);
+
+      if(Array.isArray(data.articles)){
+
+        this.state.articles = data.articles;
+
+        this.setState(this.state);
+      }
+      else{
+        console.error(data)
+        if(response){
+          response.statusCode = 400
+          response.end(data.message);
+        }
+      }
+    }
+  }
+
+}
+
+function formatDate(dates) {
+  var date = new Date(dates)
+  var hours = date.getHours();
+  var minutes = date.getMinutes();
+  var ampm = hours >= 12 ? 'pm' : 'am';
+  hours = hours % 12;
+  hours = hours ? hours : 12; // the hour '0' should be '12'
+  minutes = minutes < 10 ? '0'+minutes : minutes;
+  var strTime = hours + ':' + minutes + ' ' + ampm;
+  return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
+}
+
+
+// // Pass this content as 'props' to child components
+// const News = props => (
+//     <div>
+        
+//         <h2>News from {source.split("-").join(" ")}</h2>
+//         <div>
+//           {props.articles.map((article,index) => (
+//             <section>
+//               <h3>{article.title}</h3>
+//               <p className="author">{article.author} {article.publishedAt}</p>
+//               <img src={article.urlToImage} alt="article image" className="img-article"></img>
+//               <p>{article.description}</p>
+//               <p>{article.content}</p>
+//               <p><Link href="/story?id={index}">Read More</Link></p>
+//             </section>
+//           ))
+//           }
+//         </div>
+
+
+        // <style jsx>{`
+        //   section{
+        //     width: 50%
+        //     border: 1px solid grey;
+        //     background-color: rgb(240, 248, 255);
+        //     padding: 1em;
+        //     margin: 1em;
+        //   }
+        
+        //   .author{
+        //     font-style: italic;
+        //     font-size: 0.8em;
+        //   }
+
+        //   .img-article{
+        //     width: 25%;
+        //   }
+          
+        // `}</style>
+//     </div>
+//   );
+
+//   News.getInitialProps = async function() {
+
+//     const res = await fetch(url);
+
+//     const data = await res.json();
+
+//     //console.log(`response: ${res.status} ${res.statusText}`);
+
+//     console.log(`Show data fetched. Count: ${data.articles.length}`);
+
+//     return{
+//       articles: data.articles
+//     }
+//   }
+// export default News;
+// //62409199c79d461ea0a6fd6f83b8d44d
